5cdaa789847047ea91b4f89656160f5730cd2d97
k1LoW <k1lowxb@gmail.com>
2020-04-03 01:08:09 +0900

    Add command `frgm list`

--- cmd/root.go
+++ cmd/root.go
@@ -29,6 +29,12 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var (
+	srcPath    string
+	destPath   string
+	formatType string
+)
+
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "frgm",
